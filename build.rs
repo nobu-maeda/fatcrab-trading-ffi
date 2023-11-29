0a82987c5fe7a2735154de2ec8f33d7d9eeebbd0
Nobu Maeda <nobu.maeda@proton.me>
2023-11-29 23:52:14 +0800

    Initial commit - Working basic FFI generation

---
+++ build.rs
@@ -0,0 +1,3 @@
+fn main() {
+    uniffi::generate_scaffolding("./src/fatcrab_trading.udl").unwrap();
+}
---
+++ build.sh
@@ -0,0 +1,32 @@
+NAME="FatcrabTrading"
+APP_NAME="OceanSea"
+HEADERPATH="bindings/${NAME}FFI.h"
+TARGETDIR="target"
+OUTDIR="../${APP_NAME}/${APP_NAME}/"
+RELDIR="release"
+STATIC_LIB_NAME="libfatcrab_trading.a"
+NEW_HEADER_DIR="bindings/include"
+
+cargo build --target aarch64-apple-darwin --release
+cargo build --target aarch64-apple-ios --release
+cargo build --target aarch64-apple-ios-sim --release
+
+# UniFfi bindgen
+cargo run --bin uniffi-bindgen generate "src/fatcrab_trading.udl" --language swift --out-dir ./bindings
+
+mkdir -p "${NEW_HEADER_DIR}"
+cp "${HEADERPATH}" "${NEW_HEADER_DIR}/"
+cp "bindings/${NAME}FFI.modulemap" "${NEW_HEADER_DIR}/module.modulemap"
+
+rm -rf "${OUTDIR}/${NAME}.swift"
+cp "bindings/${NAME}.swift" "${OUTDIR}/${NAME}.swift"
+
+rm -rf "${OUTDIR}/${NAME}_framework.xcframework"
+xcodebuild -create-xcframework \
+    -library "${TARGETDIR}/aarch64-apple-darwin/${RELDIR}/${STATIC_LIB_NAME}" \
+    -headers "${NEW_HEADER_DIR}" \
+    -library "${TARGETDIR}/aarch64-apple-ios/${RELDIR}/${STATIC_LIB_NAME}" \
+    -headers "${NEW_HEADER_DIR}" \
+    -library "${TARGETDIR}/aarch64-apple-ios-sim/${RELDIR}/${STATIC_LIB_NAME}" \
+    -headers "${NEW_HEADER_DIR}" \
+    -output "${OUTDIR}/${NAME}_framework.xcframework"
